924d67338616a9fb057fbb856ae32b235532dae0
Maneesh KUMAR <maneesh.irs@gmail.com>
2020-10-01 21:12:23 +0800

    Added reaming instruction for install of esp32

--- install.sh
+++ install.sh
@@ -6,3 +6,15 @@ sudo apt-get install git wget flex bison gperf python python-pip python-setuptoo
 sudo apt-get install python3 python3-pip python3-setuptools
 sudo update-alternatives --install /usr/bin/python python /usr/bin/python3 10
 
+sudo mkdir /opt/esp
+sudo chown pi:root /opt/esp
+cd /opt/esp
+git clone --recursive https://github.com/espressif/esp-idf.git
+
+mkdir /opt/esp/.espressif
+export IDF_TOOLS_PATH=/opt/esp/.espressif
+cd /opt/esp/esp-idf
+./install.sh
+
+. /opt/esp/esp-idf/export.sh
+
